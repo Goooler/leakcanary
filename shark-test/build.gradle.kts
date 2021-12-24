b621a05a92d1488b2c721bd941fcec403a7b43f3
Goooler <wangzongler@gmail.com>
2021-12-24 16:36:26 +0800

    Convert shark-test module's build.gradle

---
+++ shark-test/build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+  kotlin("jvm")
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+  targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+  implementation(libs.kotlin.stdlib)
+  implementation(libs.assertjCore)
+  implementation(libs.junit)
+}
+
